1de3eab04189e83a7917d3a549fee67a4f815932
Hiroshi Nakao <hnakao11@gmail.com>
2018-05-02 11:58:59 -0400

    update models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Bank(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class PaymentDate(models.Model):
+     emission_date = models.DateField()
+     at_date = models.DateField()
+     post_date = models.DateField()
+
+
+class PayCheck(models.Model):
+    bank = models.ForeignKey(Bank, null=False, blank=False, related_name='bank', on_delete=models.DO_NOTHING)
+    pay_date = models.ForeignKey(PaymentDate, null=False, blank=False, on_delete=models.CASCADE, related_name='pay_date')
+    check_number = models.IntegerField(unique=True)
+    beneficiary = models.CharField(max_length=100)
+    money_value = models.DecimalField(max_digits=6, decimal_places=2)
+    concept = models.CharField(max_length=200)
+    notes = models.CharField(max_length=200)
